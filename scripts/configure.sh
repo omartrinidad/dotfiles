86323bcff08c4630f5bb72eb2bb2164d6f61b240
Omar Gutiérrez <omar.vpa@gmail.com>
2023-06-27 15:07:05 -0600

    Deleted unused \`ln\`

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,7 +5,6 @@
 mkdir ~/.vim/
 ln -sf ~/dotfiles/vim/* ~/.vim/
 ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/vim/colors/* ~/.vim/colors/
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/powerline/fonts ~/.fonts
